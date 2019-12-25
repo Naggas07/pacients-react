fbc0b1d90007a05abf99c92cc11345ca5dd11ccb
roberto araya gil <naggas@Naggas.local>
2019-12-25 17:04:25 +0100

    done version

--- src/components/listCites.js
+++ src/components/listCites.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Cite from './Cite'
 import PropTypes from 'prop-types'
 
-const ListCites = ({cites, deleteCite}) => (
+const ListCites = ({cites, deleteCite}) => {
+    const message = Object.keys(cites).length === 0 ? 'No hay citas' : 'administrar citas'
+
+    return (
     <div className="card mt-5 py-5">
         <div className="card-body">
-            <h2 className="card-tittle text-center">Administrar las citas</h2>
+            <h2 className="card-tittle text-center">{message}</h2>
             <div className="cites-list">
                 {cites.map(cite => (
                     <Cite key={cite.id} cite={cite} deleteCite={deleteCite}/>  
@@ -13,7 +16,8 @@ const ListCites = ({cites, deleteCite}) => (
             </div>
         </div>
     </div>
-)
+    )
+}
 
 ListCites.propTypes ={
     cites: PropTypes.array.isRequired,
